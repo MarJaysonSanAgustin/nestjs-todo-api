22e5cb8e07cc8e8822c46816a11ab5592c5301bf
MarJaysonSanAgustin <sanagustinjayson@gmail.com>
2021-07-31 17:56:58 +0800

    refactor: update main module Add dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  /**
+   * Load environment variables from .env file on root folder
+   */
+  config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
