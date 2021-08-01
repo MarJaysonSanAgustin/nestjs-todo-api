a3ca8b9809c9ffb9425bbc6123005d0e693cd942
MarJaysonSanAgustin <sanagustinjayson@gmail.com>
2021-08-01 22:44:19 +0800

    refactor(todo): simplify provider calls
    - Controller is now the responsible for service call validations

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, Inject, UnprocessableEntityException, NotFoundException, HttpException } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { Model, Document } from 'mongoose';
-import { ObjectId } from 'bson';
 
 import { Todo, CreateTodoDTO, ModifyTodoDTO } from './todo.schema';
 
@@ -10,61 +9,29 @@ export class TodoService {
   constructor(@Inject('TODO_MODEL') private readonly todoModel: Model<Todo>) { }
 
   async create (createTodoDTO: CreateTodoDTO): Promise<Document<Todo>> {
-    try {
-      const todo = new this.todoModel(createTodoDTO);
-      const newTodo = await todo.save();
-      return newTodo;
-    } catch (error) {
-      throw new HttpException(error?.message || 'Error', error?.status || 500);
-    }
+    const todo = new this.todoModel(createTodoDTO);
+    const newTodo = await todo.save();
+    return newTodo;
   }
 
-  async find (query: object = {}): Promise<Document<Todo>[]> {
-    try {
-      const todos = await this.todoModel.find(query);
-      return todos;
-    } catch (error) {
-      throw new HttpException(error?.message || 'Error', error?.status || 500);
-    }
+  async find (query: Partial<Todo>): Promise<Document<Todo>[]> {
+    const todos = await this.todoModel.find(query);
+    return todos;
   }
 
   async findById (todoId: string): Promise<Document<Todo>> {
-    try {
-      if (!ObjectId.isValid(todoId)) { throw new UnprocessableEntityException(`${todoId} is not a valid id.`); }
-
-      const todo = await this.todoModel.findById(todoId);
-      if (!todo) { throw new NotFoundException(`Not found`); }
-
-      return todo;
-    } catch (error) {
-      throw new HttpException(error?.message || 'Error', error?.status || 500);
-    }
+    const todo = await this.todoModel.findById(todoId);
+    return todo;
   }
 
-  async update (todoId: string, updateObj: object): Promise<Document<Todo>> {
-    try {
-      if (!ObjectId.isValid(todoId)) { throw new UnprocessableEntityException(`${todoId} is not a valid id.`); }
-
-      const updatedTodo = await this.todoModel
-        .findByIdAndUpdate(todoId, { $set: { ...updateObj } }, { new: true, runValidators: true });
-      if (!updatedTodo) { throw new NotFoundException(`Not found`); }
-
-      return updatedTodo;
-    } catch (error) {
-      throw new HttpException(error?.message || 'Error', error?.status || 500);
-    }
-  }
+  async update (todoId: string, updateTodoDTO: ModifyTodoDTO): Promise<Document<Todo>> {
+    const updatedTodo = await this.todoModel
+      .findByIdAndUpdate(todoId, { $set: { ...updateTodoDTO } }, { new: true, runValidators: true });
+    return updatedTodo;
+}
 
   async remove (todoId: string): Promise<Document<Todo>> {
-    try {
-      if (!ObjectId.isValid(todoId)) { throw new UnprocessableEntityException(`${todoId} is not a valid id.`); }
-
-      const deletedTodo = await this.todoModel.findByIdAndDelete(todoId);
-      if (!deletedTodo) { throw new NotFoundException(`Not found`); }
-
-      return deletedTodo;
-    } catch (error) {
-      throw new HttpException(error?.message || 'Error', error?.status || 500);
-    }
+    const deletedTodo = await this.todoModel.findByIdAndDelete(todoId);
+    return deletedTodo;
   }
 }
