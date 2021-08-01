16bbc1504e1cfd53542ea91af56fbfaaeb7348f1
MarJaysonSanAgustin <sanagustinjayson@gmail.com>
2021-08-01 22:41:33 +0800

    refactor(): use exception and transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './http-exception.filter';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   /**
@@ -12,6 +14,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT);
 }
 bootstrap();
