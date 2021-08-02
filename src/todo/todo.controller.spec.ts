2fb97ddce482105ab572037a4f02b2a80a64bf02
MarJaysonSanAgustin <sanagustinjayson@gmail.com>
2021-08-03 00:59:53 +0800

    test(todo): add mock service constants

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -4,23 +4,44 @@ import {
 } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { Todo } from './todo.schema';
 import { TodoService } from './todo.service';
-
-const MockTodoService = {
-  find() {
-    return [];
-  },
-  findById() {
-    return null;
-  },
-  remove() {
-    return null;
-  },
-};
+import { plainToClass } from 'class-transformer';
 
 describe('TodoController', () => {
   let todoController: TodoController;
   let todoService: TodoService;
+
+  const mockTodoService = {
+    find() {
+      return [];
+    },
+    create: jest.fn((dto) => {
+      return {
+        toObject() {
+          return plainToClass(Todo, { ...dto });
+        },
+      };
+    }),
+    findById: jest.fn((id: string) => {
+      const todo = {
+        id,
+        title: 'Test title',
+        isDone: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      return {
+        toObject() {
+          return plainToClass(Todo, { ...todo });
+        },
+      };
+    }),
+    remove() {
+      return null;
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,7 +49,7 @@ describe('TodoController', () => {
       providers: [TodoService],
     })
       .overrideProvider(TodoService)
-      .useValue(MockTodoService)
+      .useValue(mockTodoService)
       .compile();
 
     todoController = module.get<TodoController>(TodoController);
@@ -37,6 +58,27 @@ describe('TodoController', () => {
 
   it('should be defined', () => {
     expect(todoController).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return UnprocessableEntityException', async () => {
+      await expect(todoController.create(null)).rejects.toEqual(
+        new UnprocessableEntityException('Invalid todo.'),
+      );
+    });
+
+    it('should create a todo', async () => {
+      const todoDTO: Todo = {
+        id: '6107e29976a9452c30a272ac',
+        title: 'Test title',
+        isDone: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      expect(await todoController.create(todoDTO)).toEqual(
+        plainToClass(Todo, todoDTO),
+      );
+    });
   });
 
   describe('find', () => {
@@ -51,19 +93,21 @@ describe('TodoController', () => {
   describe('get', () => {
     it('should return UnprocessableEntityException for invalid ids', async () => {
       await expect(todoController.get('')).rejects.toEqual(
-        new UnprocessableEntityException(' is not a valid id.'),
+        new UnprocessableEntityException(` is not a valid id.`),
       );
     });
 
-    it('should return NotFoundException for empty results', async () => {
-      const result = null;
-      jest
-        .spyOn(todoService, 'findById')
-        .mockImplementation(async () => result);
-
-      await expect(
-        todoController.get('6107e29976a9452c30a272ac'),
-      ).rejects.toEqual(new NotFoundException());
+    it('should return a todo', async () => {
+      const result: Todo = {
+        id: '6107e29976a9452c30a272ac',
+        title: 'Test title',
+        isDone: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      expect(await todoController.get('6107e29976a9452c30a272ac')).toEqual(
+        plainToClass(Todo, result),
+      );
     });
   });
 
