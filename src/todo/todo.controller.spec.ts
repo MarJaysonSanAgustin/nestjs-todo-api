4a0b304ac4ec7bf4779525552818c51a82ad0250
MarJaysonSanAgustin <sanagustinjayson@gmail.com>
2021-08-02 23:54:45 +0800

    test(todo): add todo controller unit tests

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,18 +1,88 @@
+import {
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+
+const MockTodoService = {
+  find() {
+    return [];
+  },
+  findById() {
+    return null;
+  },
+  remove() {
+    return null;
+  },
+};
 
 describe('TodoController', () => {
-  let controller: TodoController;
+  let todoController: TodoController;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
-    }).compile();
+      providers: [TodoService],
+    })
+      .overrideProvider(TodoService)
+      .useValue(MockTodoService)
+      .compile();
 
-    controller = module.get<TodoController>(TodoController);
+    todoController = module.get<TodoController>(TodoController);
+    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(todoController).toBeDefined();
+  });
+
+  describe('find', () => {
+    it('should return an array of todos', async () => {
+      const result = [];
+      jest.spyOn(todoService, 'find').mockImplementation(async () => result);
+
+      expect(await todoController.find()).toStrictEqual(result);
+    });
+  });
+
+  describe('get', () => {
+    it('should return UnprocessableEntityException for invalid ids', async () => {
+      await expect(todoController.get('')).rejects.toEqual(
+        new UnprocessableEntityException(' is not a valid id.'),
+      );
+    });
+
+    it('should return NotFoundException for empty results', async () => {
+      const result = null;
+      jest
+        .spyOn(todoService, 'findById')
+        .mockImplementation(async () => result);
+
+      await expect(
+        todoController.get('6107e29976a9452c30a272ac'),
+      ).rejects.toEqual(new NotFoundException());
+    });
+  });
+
+  describe('remove', () => {
+    it('should return UnprocessableEntityException for invalid ids', async () => {
+      await expect(todoController.remove('')).rejects.toEqual(
+        new UnprocessableEntityException(' is not a valid id.'),
+      );
+    });
+
+    it('should return NotFoundException for empty results', async () => {
+      const result = null;
+      jest
+        .spyOn(todoService, 'findById')
+        .mockImplementation(async () => result);
+
+      await expect(
+        todoController.remove('6107e29976a9452c30a272ac'),
+      ).rejects.toEqual(new NotFoundException());
+    });
   });
 });
